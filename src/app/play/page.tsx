ae363d5e50f128a5df46f2ab00d9d320b5cb3469
0x0226 <113506290+0x0226@users.noreply.github.com>
2025-07-31 22:36:07 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -482,7 +482,7 @@ function PlayPageClient() {
           },
         });
         artPlayerRef.current.setting.update({
-          name: '设置片头',
+          name: '当前时间设为片头',
           html: '当前时间设为片头',
           icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="5" cy="12" r="2" fill="#ffffff"/><path d="M9 12L17 12" stroke="#ffffff" stroke-width="2"/><path d="M17 6L17 18" stroke="#ffffff" stroke-width="2"/></svg>',
           tooltip:
@@ -502,7 +502,7 @@ function PlayPageClient() {
           },
         });
         artPlayerRef.current.setting.update({
-          name: '设置片尾',
+          name: '当前时间设为片尾',
           html: '当前时间设为片尾',
           icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 6L7 18" stroke="#ffffff" stroke-width="2"/><path d="M7 12L15 12" stroke="#ffffff" stroke-width="2"/><circle cx="19" cy="12" r="2" fill="#ffffff"/></svg>',
           tooltip:
